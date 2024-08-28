02eac65bb53fc1a8e8f49f8097e8bf3d6b2bf535
Ben Song <blsong@unum.com>
2024-08-28 10:10:16 -0400

    ad

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
 import AudioTranscriptionDemo from './components/AudioTranscriptionDemo';
+
+function App() {
+  return (
+    <div className="App">
+      <AudioTranscriptionDemo />
+    </div>
+  );
+}
+
+export default App;
